1e70ac08d6564a75cc79d62dbe888fecf14c589a
shinnichirou.takahashi <shinnichirou.takahashi@MacBook-Air.local>
2018-08-13 12:25:03 +0900

    逆算機能

--- app/src/main/java/jp/gr/java_conf/s_takahashi/gacha/MainActivity.java
+++ app/src/main/java/jp/gr/java_conf/s_takahashi/gacha/MainActivity.java
@@ -2,6 +2,8 @@ package jp.gr.java_conf.s_takahashi.gacha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,13 +14,15 @@ import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AdRequest adRequest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        adRequest = new AdRequest.Builder().build();
         AdView adView = (AdView) findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
 
         Button btn_calc = (Button) findViewById(R.id.btnCalc);
@@ -29,27 +33,116 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onClick(View v) {
-            EditText etRate = (EditText) findViewById(R.id.editText1);
-            EditText etCount = (EditText) findViewById(R.id.editText2);
+            switch (v.getId()) {
+                case R.id.btnCalc:
+                    EditText etRate = (EditText) findViewById(R.id.editText1);
+                    EditText etCount = (EditText) findViewById(R.id.editText2);
 
-            String strRate = etRate.getText().toString();
-            String strCount= etCount.getText().toString();
+                    String strRate = etRate.getText().toString();
+                    String strCount= etCount.getText().toString();
 
-            double dRate = Double.parseDouble(strRate);
-            int iCount = Integer.parseInt(strCount);
+                    if (strRate.isEmpty() || strCount.isEmpty()) {
+                        break;
+                    }
 
-            double dResult = (1 - Math.pow(1 - dRate / 100, iCount)) * 100;
+                    double dRate = Double.parseDouble(strRate);
+                    int iCount = Integer.parseInt(strCount);
 
-            String strResult = String.format("%.3f", dResult);
-            if (dRate > 0.0 && strResult.equals("0.000")) {
-                strResult = "0.001";
+                    double dResult = (1 - Math.pow(1 - dRate / 100, iCount)) * 100;
+
+                    String strResult = String.format("%.2f", dResult);
+                    if (dRate <= 0) {
+                        strResult = "0.00";
+                    } else if (dRate >= 100) {
+                        strResult = "100.00";
+                    } else {
+                        if (strResult.equals("0.00")) {
+                            strResult = "0.01";
+                        }
+                        if (strResult.equals("100.00")) {
+                            strResult = "99.99";
+                        }
+                    }
+
+                    TextView tvResult = (TextView) findViewById(R.id.lblResult);
+                    tvResult.setText(strRate + "%のガチャを" + iCount + "回行うと" + strResult + "%の確率で当たります");
+                    break;
+
+                case R.id.sub_btnCalc:
+                    TextView tvSubResult = (TextView) findViewById(R.id.sub_lblResult);
+
+                    EditText etRate1 = (EditText) findViewById(R.id.sub_editText1);
+                    EditText etExpect = (EditText) findViewById(R.id.sub_editText2);
+                    EditText etPrice = (EditText) findViewById(R.id.sub_editText3);
+
+                    String strRate1 = etRate1.getText().toString();
+                    String strExpect = etExpect.getText().toString();
+                    String strPrice = etPrice.getText().toString();
+
+                    if (strRate1.isEmpty() || strExpect.isEmpty()) {
+                        break;
+                    }
+
+                    double dRate1 = Double.parseDouble(strRate1);
+                    double dExpect = Double.parseDouble(strExpect);
+
+                    if (!(dRate1 > 0 && dRate1 <= 100)) {
+                        tvSubResult.setText("");
+                        break;
+                    }
+                    if (!(dExpect > 0 && dExpect < 100)) {
+                        tvSubResult.setText("");
+                        break;
+                    }
+
+                    int iRevCount = (int) Math.ceil(Math.log(1.0 - dExpect / 100.0) / Math.log(1.0 - dRate1 / 100.0));
+
+                    String strSubResult = strRate1 + "%のガチャを" + String.valueOf(iRevCount) + "回行うと"  + strExpect + "%の確率で当たります";
+
+                    if (!strPrice.isEmpty()) {
+                        int iPrice = Integer.parseInt(strPrice);
+                        iPrice *= iRevCount;
+                        strSubResult = strRate1 + "%のガチャで" + String.valueOf(iPrice) + "円使うと"  + strExpect + "%の確率で当たります";;
+                    }
+
+                    tvSubResult.setText(strSubResult);
+                    break;
+
+                default:
+                    break;
             }
-            if (dRate < 100.0 && strResult.equals("100.000")) {
-                strResult = "99.999";
-            }
-
-            TextView tvResult = (TextView) findViewById(R.id.lblResult);
-            tvResult.setText(strRate + "%のガチャを" + iCount + "回行うと" + strResult + "%の確率で当たります");
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    // メニューアイテム選択イベント
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.normal:
+                setContentView(R.layout.activity_main);
+                AdView adView = (AdView) findViewById(R.id.adView);
+                adView.loadAd(adRequest);
+                Button btn_calc = (Button) findViewById(R.id.btnCalc);
+                btn_calc.setOnClickListener(new ResultButtonClickListener());
+                break;
+            case R.id.reverse:
+                setContentView(R.layout.activity_sub);
+                AdView sub_adView = (AdView) findViewById(R.id.sub_adView);
+                sub_adView.loadAd(adRequest);
+                Button sub_btn_calc = (Button) findViewById(R.id.sub_btnCalc);
+                sub_btn_calc.setOnClickListener(new ResultButtonClickListener());
+                break;
+            case R.id.finish:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
