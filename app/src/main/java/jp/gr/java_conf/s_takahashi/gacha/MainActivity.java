c0ab1349ac795d93f87c92a803e715af879810ec
s_takahashi <fdks487351@yahoo.co.jp>
2020-12-17 23:02:04 +0900

    リニューアルベース作成

--- app/src/main/java/jp/gr/java_conf/s_takahashi/gacha/MainActivity.java
+++ app/src/main/java/jp/gr/java_conf/s_takahashi/gacha/MainActivity.java
@@ -1,11 +1,15 @@
 package jp.gr.java_conf.s_takahashi.gacha;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
@@ -13,7 +17,7 @@ import com.google.android.gms.ads.AdView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextWatcher, RadioGroup.OnCheckedChangeListener {
 
     private AdRequest adRequest;
 
@@ -26,93 +30,169 @@ public class MainActivity extends AppCompatActivity {
         AdView adView = (AdView) findViewById(R.id.adView);
         adView.loadAd(adRequest);
 
-        Button btn_calc = (Button) findViewById(R.id.btnCalc);
-        btn_calc.setOnClickListener(new ResultButtonClickListener());
+        EditText editRate = (EditText) findViewById(R.id.editRate);
+        editRate.addTextChangedListener(this);
+
+        RadioGroup radioRensu = (RadioGroup) findViewById(R.id.radioRensu);
+        radioRensu.setOnCheckedChangeListener(this);
+
+        calc();
     }
 
-    class ResultButtonClickListener implements View.OnClickListener {
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.btnCalc:
-                    EditText etRate = (EditText) findViewById(R.id.editText1);
-                    EditText etCount = (EditText) findViewById(R.id.editText2);
+    }
 
-                    String strRate = etRate.getText().toString();
-                    String strCount= etCount.getText().toString();
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        calc();
+    }
 
-                    if (strRate.isEmpty() || strCount.isEmpty()) {
-                        break;
-                    }
+    @Override
+    public void afterTextChanged(Editable editable) {
+    }
 
-                    double dRate = Double.parseDouble(strRate);
-                    int iCount = Integer.parseInt(strCount);
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+        calc();
+    }
 
-                    double dResult = (1 - Math.pow(1 - dRate / 100, iCount)) * 100;
+    /* 組み合わせ */
+    private int combi(int a, int b)
+    {
+        int x = 1;
+        int y = 1;
+        for(int i = a; i > (a-b); i--) {
+            x = x * i;
+        }
+        for(int j = b; j > 0; j--) {
+            y = y * j;
+        }
+        return (x / y);
+    }
 
-                    String strResult = String.format("%.2f", dResult);
-                    if (dRate <= 0) {
-                        strResult = "0.00";
-                    } else if (dRate >= 100) {
-                        strResult = "100.00";
-                    } else {
-                        if (strResult.equals("0.00")) {
-                            strResult = "0.01";
-                        }
-                        if (strResult.equals("100.00")) {
-                            strResult = "99.99";
-                        }
-                    }
+    private void calc() {
+        // 提供割合
+        EditText etRate = (EditText) findViewById(R.id.editRate);
+        String str_rate = etRate.getText().toString();
+        double rate = Double.parseDouble(str_rate);
 
-                    TextView tvResult = (TextView) findViewById(R.id.lblResult);
-                    tvResult.setText(strRate + "%のガチャを" + iCount + "回行うと" + strResult + "%の確率で当たります");
-                    break;
+        // 特定枠
+        EditText etSpRate = (EditText) findViewById(R.id.editSpecialRate);
+        String str_special_rate = etSpRate.getText().toString();
+        double special_rate = 0;
+        if (!str_special_rate.isEmpty()) {
+            special_rate = Double.parseDouble(str_special_rate);
+        }
 
-                case R.id.sub_btnCalc:
-                    TextView tvSubResult = (TextView) findViewById(R.id.sub_lblResult);
+        // 連数
+        int gacha_num = 10;
+        RadioButton radio = (RadioButton) findViewById(R.id.radioRensu1);
+        if (radio.isChecked()) {
+            gacha_num = 1;
+        }
 
-                    EditText etRate1 = (EditText) findViewById(R.id.sub_editText1);
-                    EditText etExpect = (EditText) findViewById(R.id.sub_editText2);
-                    EditText etPrice = (EditText) findViewById(R.id.sub_editText3);
+        // 回数
+        EditText etCount = (EditText) findViewById(R.id.editKaisu);
+        String str_count= etCount.getText().toString();
+        int try_count = Integer.parseInt(str_count);
 
-                    String strRate1 = etRate1.getText().toString();
-                    String strExpect = etExpect.getText().toString();
-                    String strPrice = etPrice.getText().toString();
+        // 入力チェック
+        if (str_rate.isEmpty() || str_count.isEmpty()) {
+            findViewById(R.id.lblKaisubetu).setVisibility(View.INVISIBLE);
+            findViewById(R.id.tosenHead).setVisibility(View.INVISIBLE);
+            findViewById(R.id.tosen3).setVisibility(View.INVISIBLE);
+            findViewById(R.id.tosen2).setVisibility(View.INVISIBLE);
+            findViewById(R.id.tosen1).setVisibility(View.INVISIBLE);
+            return;
+        }
 
-                    if (strRate1.isEmpty() || strExpect.isEmpty()) {
-                        break;
-                    }
+        // 計算
+        double normal_per = rate / 100;
+        double normal_per_miss = 1 - normal_per;
+        double special_per;
+        double special_per_miss;
+        int normal_count;
+        int special_count;
 
-                    double dRate1 = Double.parseDouble(strRate1);
-                    double dExpect = Double.parseDouble(strExpect);
+        if (str_special_rate.isEmpty()) {
+            special_per = normal_per;
+            special_per_miss = normal_per_miss;
+            normal_count = gacha_num * try_count;
+            special_count = 0;
+        } else {
+            special_per = special_rate / 100;
+            special_per_miss = 1 - special_per;
+            normal_count = (gacha_num - 1) * try_count;
+            special_count = try_count;
+        }
 
-                    if (!(dRate1 > 0 && dRate1 <= 100)) {
-                        tvSubResult.setText("");
-                        break;
-                    }
-                    if (!(dExpect > 0 && dExpect < 100)) {
-                        tvSubResult.setText("");
-                        break;
-                    }
-
-                    int iRevCount = (int) Math.ceil(Math.log(1.0 - dExpect / 100.0) / Math.log(1.0 - dRate1 / 100.0));
-
-                    String strSubResult = strRate1 + "%のガチャを" + String.valueOf(iRevCount) + "回行うと"  + strExpect + "%の確率で当たります";
-
-                    if (!strPrice.isEmpty()) {
-                        int iPrice = Integer.parseInt(strPrice);
-                        iPrice *= iRevCount;
-                        strSubResult = strRate1 + "%のガチャで" + String.valueOf(iPrice) + "円使うと"  + strExpect + "%の確率で当たります";;
-                    }
-
-                    tvSubResult.setText(strSubResult);
-                    break;
-
-                default:
-                    break;
+        /* N回当たり */
+        double[] result = {0, 0, 0, 0};
+        for (int i = 0; i < result.length; i++) {
+            for (int n = 0; n <= i; n++) {
+                int s = i - n;
+                if (normal_count >= n && special_count >= s) {
+                    double normal_result  = (Math.pow(normal_per,  n) * Math.pow(normal_per_miss,  normal_count  - n)) * combi(normal_count,  n);
+                    double special_result = (Math.pow(special_per, s) * Math.pow(special_per_miss, special_count - s)) * combi(special_count, s);
+                    result[i] += normal_result * special_result;
+                }
             }
         }
+
+        /* N回以上当たり */
+        double[] result_over = {1, 1, 1, 1};
+        for (int i = 1; i < result_over.length; i++) {
+            for (int j = 0; j < i; j++) {
+                result_over[i] -= result[j];
+            }
+        }
+
+        /* 表示用にパーセント表記にする */
+        for (int i = 0; i < result.length; i++) {
+            result[i] = result[i] * 100;
+            if (result[i] < 0.00001) {
+                result[i] = 0.00001;
+            }
+            if (result[i] > 99.99999) {
+                result[i] = 99.99999;
+            }
+            result_over[i] = result_over[i] * 100;
+            if (result_over[i] < 0.00001) {
+                result_over[i] = 0.00001;
+            }
+            if (result_over[i] > 99.99999) {
+                result_over[i] = 99.99999;
+            }
+        }
+
+        /* 結果表示 */
+        String result_msg = "";
+        result_msg  = "提供割合が" + str_rate + "%";
+        if (!str_special_rate.isEmpty()) {
+            result_msg += "（特定枠が" + special_rate + "%)";
+        }
+        result_msg += "の";
+        if (gacha_num > 1) {
+            result_msg += gacha_num + "連";
+        }
+        result_msg += "ガチャを" + try_count + "回まわした場合に1つ以上当選する確率は" + String.format("%.5f", result_over[1]) + "%です";
+
+        TextView tvResult = (TextView) findViewById(R.id.lblResult);
+        tvResult.setText(result_msg);
+
+        findViewById(R.id.lblKaisubetu).setVisibility(View.VISIBLE);
+        findViewById(R.id.tosenHead).setVisibility(View.VISIBLE);
+        findViewById(R.id.tosen3).setVisibility(View.VISIBLE);
+        findViewById(R.id.tosen2).setVisibility(View.VISIBLE);
+        findViewById(R.id.tosen1).setVisibility(View.VISIBLE);
+        ((TextView)findViewById(R.id.textTosenNRate3)).setText(String.format("%.5f", result[3]));
+        ((TextView)findViewById(R.id.textTosenNRate2)).setText(String.format("%.5f", result[2]));
+        ((TextView)findViewById(R.id.textTosenNRate1)).setText(String.format("%.5f", result[1]));
+        ((TextView)findViewById(R.id.textTosenNOverRate3)).setText(String.format("%.5f", result_over[3]));
+        ((TextView)findViewById(R.id.textTosenNOverRate2)).setText(String.format("%.5f", result_over[2]));
+        ((TextView)findViewById(R.id.textTosenNOverRate1)).setText(String.format("%.5f", result_over[1]));
     }
 
     @Override
@@ -126,20 +206,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.normal:
-                setContentView(R.layout.activity_main);
-                AdView adView = (AdView) findViewById(R.id.adView);
-                adView.loadAd(adRequest);
-                Button btn_calc = (Button) findViewById(R.id.btnCalc);
-                btn_calc.setOnClickListener(new ResultButtonClickListener());
-                break;
-            case R.id.reverse:
-                setContentView(R.layout.activity_sub);
-                AdView sub_adView = (AdView) findViewById(R.id.sub_adView);
-                sub_adView.loadAd(adRequest);
-                Button sub_btn_calc = (Button) findViewById(R.id.sub_btnCalc);
-                sub_btn_calc.setOnClickListener(new ResultButtonClickListener());
-                break;
             case R.id.finish:
                 finish();
                 break;
